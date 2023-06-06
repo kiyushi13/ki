23ce1dd7f72109ee0e5acda0743db858dd2ee90e
kiyushi13 <135688610+kiyushi13@users.noreply.github.com>
2023-06-06 09:41:42 +0300

    Add files via upload

---
+++ 2Одномерные массивы/2Одномерные массивы/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2Одномерные_массивы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во чисел для формирования случайного массива:");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand.Next(15);
+                Console.WriteLine(i + ")" + arr[i]);
+            }
+            Console.WriteLine(CheckArray(arr));
+            Console.ReadKey();
+        }
+
+        static int CheckArray(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+                if ((a[i] + a[i + 1]) % 2 == 0)
+                    return i + 1;
+            return 0;
+        }
+    }
+}
