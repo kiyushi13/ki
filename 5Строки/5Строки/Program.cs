ea0672d34808e337164142aedbba975501007935
Kiyushi <135688610+kiyushi13@users.noreply.github.com>
2023-06-07 13:08:22 +0300

    Add files via upload

---
+++ 5Строки/5Строки/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _5Строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку S: ");
+            string s = Console.ReadLine();
+
+            Console.Write("Введите строку S1: ");
+            string s1 = Console.ReadLine();
+
+            Console.Write("Введите строку S2: ");
+            string s2 = Console.ReadLine();
+
+            string result = s.Replace(s1, s2); // замена вхождений строки S1 на строку S2 в строке S
+
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+}
